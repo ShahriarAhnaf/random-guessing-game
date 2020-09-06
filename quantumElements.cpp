8e2885bbde93a7d291e3848f0b2a394847602a39
Ahnaf Shahriar <70302254+ShahriarAhnaf@users.noreply.github.com>
2020-09-06 16:07:54 -0400

    Add files via upload

---
+++ quantumElements.cpp
@@ -0,0 +1,71 @@
+// quantumElements.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <random>
+#include <cstdlib>
+#include <ctime>
+#include <iomanip>
+
+using namespace std;
+
+/*
+static std::random_device rd; 
+static std::mt19937 rng(rd());
+
+int RandElement()
+{
+    static std::uniform_int_distribution<int> elements(1, 20); // random dice
+    return elements(rng); // use rng as a generator
+}
+*/
+//the game developer way?
+
+int main()
+{
+   
+    int compElement = 0;
+    int tries = 0;
+    string elements[20] = { "hydrogen", "helium", "lithium", "berylium", "boron", "carbon", "nitrogen", "oxygen", "flourine", "neon",
+                    "sodium", "magnesium", "aluminium", "silicon", "phosphorus", "sulfur", "chlorine", "argon" };
+    string guess = "";
+    
+    //using srand
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
+//making the random number
+    string compGuess = elements[rand() % 19]; // to make the rand int compatible with the array.
+    
+
+
+    cout <<setw(40)<<"first 20 elements guesser\n";
+    cout << "\n The computer will guess from these elements : \n ";
+    
+    for (int i = 0; i < 19; i++)
+    {
+        if (i % 2 != 0)
+        {
+            cout << elements[i] << " \n";
+        }
+        else
+        {
+            cout << elements[i]<< "\t" ;
+        }
+    }
+
+
+    cout << "\n Guess the same element as the computer! : ";
+    
+   while (guess != compGuess)
+    {
+        if (tries > 0)
+        {
+            cout << tries << " try. keep trying! :";
+        }
+        cin >> guess;
+        tries++;
+    }
+
+   cout << "\n YOU WON! YOU CORRECTLY CAUGHT THE COMPUTER GUESSING" << compGuess;
+
+
+}
